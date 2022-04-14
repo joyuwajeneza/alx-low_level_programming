540c140865b4d41b65f48d5c3ea12b56dee1a101
joyuwajeneza <dajoyuwajeneza@gmail.com>
2022-04-14 09:18:19 -0700

    MY commit message

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+print f ( "size of a char: %d byte(s)\n", sizeof(char));
+print f ( "size of an int: %d byte(s)\n", size(int));
+print f ( " size of  a long int: %d byte(s)\n",sizeof(long long int));
+print f ( " size of  a float: %d byte(s)\n",size(float));
+return (0);
+}
