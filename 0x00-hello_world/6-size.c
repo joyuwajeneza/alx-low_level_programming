61d9224e9896ff98f584e180fb3b219f08332fb0
joyuwajeneza <dajoyuwajeneza@gmail.com>
2022-04-14 09:38:13 -0700

    My commit message

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- * main -Entry point
- *
- * Return: always 0 (success)
- */
+* main -Entry point
+*
+* Return: always 0 (success)
+*/
 int main(void)
 {
-print f ( "size of a char: %d byte(s)\n", sizeof(char));
-print f ( "size of an int: %d byte(s)\n", sizeof(int));
-print f ( " size of  a long int: %d byte(s)\n",sizeof( long int));
-print f ( " size of a long long int: %d byte(s)\n",sizeof(long long int));
-print f ( " size of  a float: %d byte(s)\n",sizeof(float));
+print f ( "size of a char: %d byte(s)\n", sizeof (char));
+print f ( "size of an int: %d byte(s)\n", sizeof (int));
+print f ( " size of  a long int: %d byte(s)\n",sizeof (long int));
+print f ( " size of a long long int: %d byte(s)\n",sizeof (long long int));
+print f ( " size of  a float: %d byte(s)\n",sizeof (float));
 return (0);
 }
