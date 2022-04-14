08231a2a456849df78da2d28ca460c38318e5fc6
joyuwajeneza <dajoyuwajeneza@gmail.com>
2022-04-14 07:54:51 -0700

    My commit message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
 * main - entry point
 * 
@@ -6,6 +6,7 @@
 */
 int main(void)
 {
+
 puts("\"programming is like building a multilingual puzzle");
  
 return (0);
